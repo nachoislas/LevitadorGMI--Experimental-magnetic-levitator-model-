1ecf76799197a81f4971d172885e07452c403ddd
Javier Mosconi <72772049+Javim24@users.noreply.github.com>
2021-10-14 18:49:26 -0300

    Create compensacion_derivador_con_mcp660.m

---
+++ MATLAB/Estimador/compensacion_derivador_con_mcp660.m
@@ -0,0 +1,47 @@
+s = tf('s');
+p1 =2 * pi * 20;           %primer polo del operacional en rads
+p2 = 2* pi * 60e6;          %segundo polo del operacional en rads
+Aw = 3.16227766 * 1e6 * 1 / ((1+s/p1) * (1+s/p2)) ;   %transferencia del operacional
+
+
+R1 = 25e3;                      
+C = 1e-6;                   %R1 y C para tau = 30 ms
+R2 = 0;                 %por ahora lo considero 0
+
+H = (1 + s*C*R2) / (1 + s*C*(R1+R2));   
+
+
+R2 = 10; % calculada para polo en 16 kHz
+
+Hcomp = (1 + s*C*R2) / (1 + s*C*(R1+R2));   
+
+bode(Aw, 1/H, 1/Hcomp)
+
+
+%%
+
+GH = Aw*Hcomp;
+
+margin(GH)
+
+%%
+
+step(GH)
+
+%%
+tlc = Aw/ ( 1+ Aw * Hcomp);
+
+bode(tlc)
+
+%%
+
+step(tlc)
+
+%%
+
+Gmenos = -s*C*R1/(1+s*C*(R2+R1));
+tlcmenos = minreal(Gmenos*tlc) * 1 / (1+s/10e3);
+
+zpk(tlcmenos)
+
+bode(tlcmenos)

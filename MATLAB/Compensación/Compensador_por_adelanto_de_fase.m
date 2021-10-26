ab731606eb62fb6374f796efd456ba896fdb9d27
nachoislas <nacho.th.21@gmail.com>
2021-10-26 20:35:44 -0300

    xdd

--- MATLAB/Compensación/Compensador_por_adelanto_de_fase.m
+++ MATLAB/Compensación/Compensador_por_adelanto_de_fase.m
@@ -1,16 +1,15 @@
 %% Controlador po adelanto de fase
 s = tf('s');
-m = 1;                                             %masa que levanta el electroimán
-wp = 99; %para y=4m
-L = 22.64;
+m = 10;                                             %masa que levanta el electroimán
+L = 22.64/1000;
 Kin = 0.32;
-H = 53.33;
+H = 53.33/1000;
 R = 0.2;
 y0=2/1000;
 k=1.76715e-5;
-Gplanta = (2/y0)*(sqrt(k*9.8/m))/(s^2-(2*9.8/y0));
+Gplanta = -(2/y0)*(sqrt(k*9.8/m))/(s^2-(2*9.8/y0));
 %Gplanta = (1342e-6/sqrt(m)) / (1 - (s/70)^2);
-GiL = (Kin/H) / (1 + s/(L/R));                  
+GiL = (Kin/H) / (1 + s/(R/L));                  
 Hestim = 259.6 / ((1 + s/1e3) * (1 + s/60e3)^2 );
 
 Gtotal = Gplanta * GiL * Hestim;
@@ -29,8 +28,10 @@ figure(3)
 rlocus(Gtotal)
 
 %% Agregando el compensador por adelanto de fase diseñado
-Kc = 10;
+Kc = 20;
 Gc = Kc * (20.346 * (s + 47.4)/(s + 965.4))^2;
+
+ 
 
 %% Bode de la planta con compensador
 Gcompensado = Gtotal * Gc;
